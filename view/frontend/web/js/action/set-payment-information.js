c5e4dc25098bb77face5ad313375e29602c998fd
Bob Kersten <bob@cardgate.com>
2017-11-22 08:50:47 +0100

    better reloading indicator

--- view/frontend/web/js/action/set-payment-information.js
+++ view/frontend/web/js/action/set-payment-information.js
@@ -5,40 +5,29 @@
  */
 define(
 	[
-		'Magento_Checkout/js/model/quote',
 		'mage/url',
 		'mage/storage',
-		'Magento_Checkout/js/model/error-processor',
-		'Magento_Customer/js/model/customer',
 		'Magento_Checkout/js/action/get-totals',
-		'Magento_Checkout/js/model/full-screen-loader',
-		'Magento_Checkout/js/model/resource-url-manager'
+		'Magento_Checkout/js/model/totals'
 	],
 	function(
-		quote,
 		urlBuilder,
 		storage,
-		errorProcessor,
-		customer,
 		getTotalsAction,
-		fullScreenLoader,
-		resourceUrlManager
+		totals
 	) {
 		'use strict';
 
 		return function( messageContainer, paymentData ) {
-			fullScreenLoader.startLoader();
+			totals.isLoading( true );
 			return storage.get( urlBuilder.build( 'cardgate/payment/updatepm?pm=' + paymentData.method ) )
 				.fail(
 					function( response ) {
+						totals.isLoading( false );
 					}
 				).done(
 					function() {
 						getTotalsAction([]);
-					}
-				).always(
-					function() {
-						fullScreenLoader.stopLoader();
 					}
 				)
 			;
