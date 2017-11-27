73d09935a1019a8b8c8b825b02c69be34951f6c7
Bob Kersten <bob@cardgate.com>
2017-11-27 12:06:36 +0100

    language strings and autoloader changes

--- registration.php
+++ registration.php
@@ -4,6 +4,7 @@
  * All rights reserved.
  * See LICENSE for license details.
  */
+namespace Cardgate\Payment;
 
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
@@ -14,17 +15,23 @@
 /**
  * Dynamic payment method class loader.
  */
-spl_autoload_register( function( $sClassName ) {
-	$sClassPrefix = 'Cardgate\Payment\Model\PaymentMethod';
-	if ( FALSE !== strstr( $sClassName, $sClassPrefix ) ) {
-		$sPM = substr( $sClassName, strlen( $sClassPrefix ) + 1 );
-		$oFileSystem = \Magento\Framework\App\ObjectManager::getInstance()->get( \Magento\Framework\Filesystem::class );
-		$oDirectory = $oFileSystem->getDirectoryWrite( \Magento\Framework\App\Filesystem\DirectoryList::TMP );
-		$sVersion = \Magento\Framework\App\ObjectManager::getInstance()->get( \Magento\Framework\Module\ModuleListInterface::class )->getOne( 'Cardgate_Payment' )['setup_version'];
-		$sFilename = "paymentmethod_cardgate_{$sVersion}_{$sPM}.php";
-		if ( ! $oDirectory->isFile( $sFilename ) ) {
-			$oDirectory->writeFile( $sFilename, "<?php\nnamespace Cardgate\\Payment\\Model\\PaymentMethod;\nclass {$sPM} extends \\Cardgate\\Payment\\Model\\PaymentMethod\\generic {\n}\n" );
+class Registration {
+
+	public static function autoload( $sClassName ) {
+		$sClassPrefix = 'Cardgate\Payment\Model\PaymentMethod';
+		if ( FALSE !== strstr( $sClassName, $sClassPrefix ) ) {
+			$sPM = substr( $sClassName, strlen( $sClassPrefix ) + 1 );
+			$oFileSystem = \Magento\Framework\App\ObjectManager::getInstance()->get( \Magento\Framework\Filesystem::class );
+			$oDirectory = $oFileSystem->getDirectoryWrite( \Magento\Framework\App\Filesystem\DirectoryList::TMP );
+			$sVersion = \Magento\Framework\App\ObjectManager::getInstance()->get( \Magento\Framework\Module\ModuleListInterface::class )->getOne( 'Cardgate_Payment' )['setup_version'];
+			$sFilename = "paymentmethod_cardgate_{$sVersion}_{$sPM}.php";
+			if ( ! $oDirectory->isFile( $sFilename ) ) {
+				$oDirectory->writeFile( $sFilename, "<?php\nnamespace Cardgate\\Payment\\Model\\PaymentMethod;\nclass {$sPM} extends \\Cardgate\\Payment\\Model\\PaymentMethod\\generic {\n}\n" );
+			}
+			require $oDirectory->getAbsolutePath( $sFilename );
 		}
-		require $oDirectory->getAbsolutePath( $sFilename );
 	}
-} );
+
+}
+
+spl_autoload_register( [ '\Cardgate\Payment\Registration', 'autoload' ] );
